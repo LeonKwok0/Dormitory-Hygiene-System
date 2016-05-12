731a486fa507aa000ea54691a7fe1866431e83f6
LeonKwok <leonGuo08@gmail.com>
2016-05-12 14:10:38 +0800

    eddit submit buttion event in duct panel

--- DemoProject/src/gui/MainDemo.java
+++ DemoProject/src/gui/MainDemo.java
@@ -50,7 +50,7 @@ public class MainDemo extends JFrame {
 	private String buliding;
 	private String room;
 	private JLabel checkPoints;
-	
+
 	private Vector columnHeads;
 	private Vector rowdata;
 	private JLabel labelReturnResult;
@@ -58,6 +58,8 @@ public class MainDemo extends JFrame {
 	private JScrollPane scrollPane;
 	private JTable tableAlarm;
 	private JScrollPane scrollPane_alram;
+	private boolean pointIsTrue = true;
+
 	/**
 	 * Launch the application.
 	 */
@@ -74,7 +76,6 @@ public class MainDemo extends JFrame {
 		});
 	}
 
-	
 	/**
 	 * Create the frame.
 	 */
@@ -190,29 +191,29 @@ public class MainDemo extends JFrame {
 
 		JLabel lblNewLabel_6 = new JLabel("Week");
 
-		
-		
 		/**
 		 * submit button event
 		 */
 		JButton btnNewButton = new JButton("Submit");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// cast string point to int 
+				// cast string point to int
 				CastPoints(textFieldPoint.getText());
-				
+
 				// comfirm dialog
-				String message = "Dormitory: " + buliding + room + "\n" + "reason: " + reasons1
-						+ textFieldOtherReason.getText() + "\n" + "time: " + time + "\n" + "Duct points:"
-						+ deductPoints;
-				int option = JOptionPane.showConfirmDialog(null, message,"Are you sure?",
-						JOptionPane.OK_CANCEL_OPTION);
-				if(option==0){
-					System.out.println(option);
-					addRecord();
+				if (pointIsTrue) {
+					String message = "Dormitory: " + buliding + room + "\n" + "reason: " + reasons1
+							+ textFieldOtherReason.getText() + "\n" + "time: " + time + "\n" + "Duct points:"
+							+ deductPoints;
+					int option = JOptionPane.showConfirmDialog(null, message, "Are you sure?",
+							JOptionPane.OK_CANCEL_OPTION);
+					if (option == 0) {
+						System.out.println(option);
+						addRecord();
+					}
 				}
 			}
-				
+
 		});
 
 		JSeparator separator = new JSeparator();
@@ -224,96 +225,91 @@ public class MainDemo extends JFrame {
 		JLabel lblNewLabel_7 = new JLabel("");
 
 		checkPoints = new JLabel("");
-		
+
 		labelReturnResult = new JLabel("Result:");
 
 		GroupLayout gl_panel = new GroupLayout(panel);
-		gl_panel.setHorizontalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-						.addComponent(separator, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 438, Short.MAX_VALUE)
+		gl_panel.setHorizontalGroup(gl_panel.createParallelGroup(
+				Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup().addContainerGap().addGroup(gl_panel
+						.createParallelGroup(Alignment.LEADING).addComponent(separator, Alignment.TRAILING,
+								GroupLayout.DEFAULT_SIZE, 438, Short.MAX_VALUE)
 						.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
-							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_panel.createParallelGroup(Alignment.LEADING, false)
-									.addComponent(lblNewLabel_2)
-									.addGroup(gl_panel.createSequentialGroup()
-										.addComponent(lblNewLabel_4)
-										.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-										.addComponent(lblNewLabel_5)))
-								.addComponent(lblNewLabel)
-								.addComponent(lblNewLabel_3))
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-								.addComponent(comboBoxBuliding, 0, 117, Short.MAX_VALUE)
-								.addComponent(comboBoxTime, 0, 117, Short.MAX_VALUE)
-								.addComponent(comboBoxReason, 0, 117, Short.MAX_VALUE)
-								.addComponent(textFieldPoint, GroupLayout.DEFAULT_SIZE, 117, Short.MAX_VALUE))
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_panel.createSequentialGroup()
-									.addComponent(lblNewLabel_1)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(comboBoxRoom, GroupLayout.PREFERRED_SIZE, 145, GroupLayout.PREFERRED_SIZE))
-								.addComponent(textFieldOtherReason, GroupLayout.DEFAULT_SIZE, 248, Short.MAX_VALUE)
-								.addComponent(lblNewLabel_6)
-								.addGroup(gl_panel.createSequentialGroup()
-									.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-										.addComponent(lblNewLabel_7)
+								.addGroup(gl_panel
+										.createParallelGroup(Alignment.LEADING).addGroup(gl_panel
+												.createParallelGroup(Alignment.LEADING, false)
+												.addComponent(lblNewLabel_2)
+												.addGroup(gl_panel.createSequentialGroup().addComponent(lblNewLabel_4)
+														.addPreferredGap(ComponentPlacement.RELATED,
+																GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+														.addComponent(lblNewLabel_5)))
+										.addComponent(lblNewLabel).addComponent(lblNewLabel_3))
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+										.addComponent(comboBoxBuliding, 0, 117, Short.MAX_VALUE)
+										.addComponent(comboBoxTime, 0, 117, Short.MAX_VALUE)
+										.addComponent(comboBoxReason, 0, 117, Short.MAX_VALUE)
+										.addComponent(textFieldPoint, GroupLayout.DEFAULT_SIZE, 117, Short.MAX_VALUE))
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+										.addGroup(gl_panel.createSequentialGroup().addComponent(lblNewLabel_1)
+												.addPreferredGap(ComponentPlacement.RELATED).addComponent(comboBoxRoom,
+														GroupLayout.PREFERRED_SIZE, 145, GroupLayout.PREFERRED_SIZE))
+										.addComponent(textFieldOtherReason, GroupLayout.DEFAULT_SIZE, 248,
+												Short.MAX_VALUE)
+										.addComponent(lblNewLabel_6)
 										.addGroup(gl_panel.createSequentialGroup()
-											.addGap(29)
-											.addComponent(checkPoints)))
-									.addPreferredGap(ComponentPlacement.RELATED, 131, Short.MAX_VALUE)
-									.addComponent(btnNewButton))))
-						.addComponent(labelReturnResult))
-					.addContainerGap())
-		);
-		gl_panel.setVerticalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel)
-						.addComponent(comboBoxBuliding, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_1)
-						.addComponent(comboBoxRoom, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel_2)
-						.addComponent(comboBoxReason, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(textFieldOtherReason, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel_4)
-						.addComponent(comboBoxTime, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_5)
-						.addComponent(lblNewLabel_6))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(textFieldPoint, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_3)
-						.addComponent(btnNewButton)
-						.addComponent(lblNewLabel_7)
-						.addComponent(checkPoints))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(separator, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(labelReturnResult)
-					.addContainerGap(123, Short.MAX_VALUE))
-		);
+												.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+														.addComponent(lblNewLabel_7)
+														.addGroup(gl_panel.createSequentialGroup().addGap(29)
+																.addComponent(checkPoints)))
+												.addPreferredGap(ComponentPlacement.RELATED, 131, Short.MAX_VALUE)
+												.addComponent(btnNewButton))))
+						.addComponent(labelReturnResult)).addContainerGap()));
+		gl_panel.setVerticalGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup().addContainerGap()
+						.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE).addComponent(lblNewLabel)
+								.addComponent(comboBoxBuliding, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+										GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblNewLabel_1).addComponent(comboBoxRoom, GroupLayout.PREFERRED_SIZE,
+										GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE).addComponent(lblNewLabel_2)
+								.addComponent(comboBoxReason, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+										GroupLayout.PREFERRED_SIZE)
+								.addComponent(textFieldOtherReason, GroupLayout.PREFERRED_SIZE,
+										GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE).addComponent(lblNewLabel_4)
+								.addComponent(comboBoxTime, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+										GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblNewLabel_5).addComponent(lblNewLabel_6))
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+								.addComponent(textFieldPoint, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+										GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblNewLabel_3).addComponent(btnNewButton).addComponent(lblNewLabel_7)
+								.addComponent(checkPoints))
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addComponent(separator, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED).addComponent(labelReturnResult)
+						.addContainerGap(123, Short.MAX_VALUE)));
 		panel.setLayout(gl_panel);
-		
+
+		/**
+		 * code for rank tag
+		 */
 		JPanel panelRank = new JPanel();
 		tabbedPane.addTab("Ranking", null, panelRank, null);
 		panelRank.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel panel_1 = new JPanel();
 		panelRank.add(panel_1, BorderLayout.NORTH);
-		
+
 		JLabel lblNewLabel_8 = new JLabel("This is the dormitory Rank order by total points:");
 		lblNewLabel_8.setHorizontalAlignment(SwingConstants.CENTER);
-		
+
 		JButton buttonRefresh = new JButton("Refresh");
 		buttonRefresh.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -321,149 +317,135 @@ public class MainDemo extends JFrame {
 			}
 		});
 		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
-		gl_panel_1.setHorizontalGroup(
-			gl_panel_1.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_1.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblNewLabel_8, GroupLayout.PREFERRED_SIZE, 319, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 127, Short.MAX_VALUE)
-					.addComponent(buttonRefresh))
-		);
-		gl_panel_1.setVerticalGroup(
-			gl_panel_1.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_panel_1.createParallelGroup(Alignment.BASELINE)
-					.addComponent(lblNewLabel_8, GroupLayout.DEFAULT_SIZE, 22, Short.MAX_VALUE)
-					.addComponent(buttonRefresh))
-		);
+		gl_panel_1
+				.setHorizontalGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel_1.createSequentialGroup().addContainerGap()
+								.addComponent(lblNewLabel_8, GroupLayout.PREFERRED_SIZE, 319,
+										GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.RELATED, 127, Short.MAX_VALUE)
+								.addComponent(buttonRefresh)));
+		gl_panel_1.setVerticalGroup(gl_panel_1.createParallelGroup(Alignment.LEADING).addGroup(Alignment.TRAILING,
+				gl_panel_1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel_8, GroupLayout.DEFAULT_SIZE, 22, Short.MAX_VALUE)
+						.addComponent(buttonRefresh)));
 		panel_1.setLayout(gl_panel_1);
-		
+
 		scrollPane = new JScrollPane();
 		panelRank.add(scrollPane, BorderLayout.CENTER);
-		
-		
+
 		tableRank = new JTable();
 		scrollPane.setViewportView(tableRank);
 
 		JPanel panelAlarn = new JPanel();
 		tabbedPane.addTab("Alarm", null, panelAlarn, null);
 		panelAlarn.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel panelAlaram_North = new JPanel();
 		panelAlarn.add(panelAlaram_North, BorderLayout.NORTH);
-		
+
 		JLabel labelalram = new JLabel("Points uder 60 will be shown here:");
-		
+
 		JButton btnNewButton_1 = new JButton("Refresh");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				alarm();
 			}
 		});
-		
-		// group layout for alram 
+
+		// group layout for alram
 		GroupLayout gl_panelAlaram_North = new GroupLayout(panelAlaram_North);
-		gl_panelAlaram_North.setHorizontalGroup(
-			gl_panelAlaram_North.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panelAlaram_North.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(labelalram)
-					.addPreferredGap(ComponentPlacement.RELATED, 224, Short.MAX_VALUE)
-					.addComponent(btnNewButton_1)
-					.addContainerGap())
-		);
+		gl_panelAlaram_North.setHorizontalGroup(gl_panelAlaram_North.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panelAlaram_North.createSequentialGroup().addContainerGap().addComponent(labelalram)
+						.addPreferredGap(ComponentPlacement.RELATED, 224, Short.MAX_VALUE).addComponent(btnNewButton_1)
+						.addContainerGap()));
 		gl_panelAlaram_North.setVerticalGroup(
-			gl_panelAlaram_North.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_panelAlaram_North.createParallelGroup(Alignment.BASELINE)
-					.addComponent(labelalram, GroupLayout.DEFAULT_SIZE, 22, Short.MAX_VALUE)
-					.addComponent(btnNewButton_1))
-		);
+				gl_panelAlaram_North.createParallelGroup(Alignment.LEADING).addGroup(Alignment.TRAILING,
+						gl_panelAlaram_North.createParallelGroup(Alignment.BASELINE)
+								.addComponent(labelalram, GroupLayout.DEFAULT_SIZE, 22, Short.MAX_VALUE)
+								.addComponent(btnNewButton_1)));
 		panelAlaram_North.setLayout(gl_panelAlaram_North);
-		
+
 		scrollPane_alram = new JScrollPane();
 		panelAlarn.add(scrollPane_alram, BorderLayout.CENTER);
 		tableAlarm = new JTable();
 		scrollPane_alram.setViewportView(tableAlarm);
-		
-		
+
+		// add jtabbedpane to frame
 		contentPane.add(tabbedPane);
 	}
 
-	
-	
-	
-	
-	
-	
-	
 	/**
 	 * CastPoints(String points) is used to cast String Type(come from jtextfild
 	 * input) to int
 	 */
 	private int CastPoints(String points) {
 		checkPoints.setText("");
+		pointIsTrue = true;
 		try {
 			this.deductPoints = Integer.parseInt(points);
 			if (this.deductPoints <= 0 || this.deductPoints >= 100) {
 				checkPoints.setText("Exceed Range");
+				pointIsTrue = false;
 			}
 		} catch (NumberFormatException e1) {
 			checkPoints.setText(" Only Enter Interger");
+			pointIsTrue = false;
 		} catch (Exception e) {
 			checkPoints.setText(" UnKnwon Error");
+			pointIsTrue = false;
 		}
 		return deductPoints;
 
 	}
-	
-	
+
 	/**
-	 * add deductrecord and return result 
+	 * add deductrecord and return result
 	 */
-	private void addRecord(){
-		//set duct points record 
-		dor.setDorId(buliding+room);
-		dor.setReasons(reasons1+""+textFieldOtherReason.getText());
+	private void addRecord() {
+		// set duct points record
+		dor.setDorId(buliding + room);
+		dor.setReasons(reasons1 + "" + textFieldOtherReason.getText());
 		dor.setDeductPoints(deductPoints);
 		dor.setTime(time);
-		Deduct deduct=new Deduct();
-		
-		
-		//add record to database
+		Deduct deduct = new Deduct();
+
+		// add record to database
 		try {
-			if(deduct.idExists(dor)){
+			if (deduct.idExists(dor)) {
 				deduct.update(dor);
 				deduct.checkCurrentPoints(dor);
-				labelReturnResult.setText("Result:"+"Deduct Successfully !"+"\n"+
-												"Dormitory:"+dor.getDorId()+" Current Points: "+deduct.getCurrentPoints());
-			}else {
-				labelReturnResult.setText("Result:"+"\n"+"Error: No this Dormitory!");
+				labelReturnResult.setText("Result:" + "Deduct Successfully !" + "\n" + "Dormitory:" + dor.getDorId()
+						+ " Current Points: " + deduct.getCurrentPoints());
+			} else {
+				labelReturnResult.setText("Result:" + "\n" + "Error: No this Dormitory!");
 			}
-			
+
 		} catch (SQLException e2) {
 			e2.printStackTrace();
-		}			
+		}
 	}
+
 	/**
 	 * this method is used to show table from Database
 	 */
-	public void rank(){
-		QueryTable query=new QueryTable();
+	public void rank() {
+		QueryTable query = new QueryTable();
 		query.Query("SELECT dorId, address, points FROM Dormitory ORDER BY points DESC");
 		columnHeads = query.getColumnHeads();
 		rowdata = query.getRowdata();
-		tableRank = new JTable(rowdata,columnHeads );
+		tableRank = new JTable(rowdata, columnHeads);
 		scrollPane.setViewportView(tableRank);
 		query.closeConn();
 	}
-	
-	public void alarm(){
-		 Vector columnHeads2;
-		 Vector rowdata2;
-		QueryTable query=new QueryTable();
+
+	public void alarm() {
+		Vector columnHeads2;
+		Vector rowdata2;
+		QueryTable query = new QueryTable();
 		query.Query("SELECT dorId, address, points FROM Dormitory WHERE points<60 ");
 		columnHeads2 = query.getColumnHeads();
 		rowdata2 = query.getRowdata();
-		tableAlarm = new JTable(rowdata2,columnHeads2 );
+		tableAlarm = new JTable(rowdata2, columnHeads2);
 		scrollPane_alram.setViewportView(tableAlarm);
 		query.closeConn();
 	}

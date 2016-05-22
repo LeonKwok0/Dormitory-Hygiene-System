8cfe78d4953c2acfd2f6fbc06487014dd2fe426a
LeonKwok <leonGuo08@gmail.com>
2016-05-22 15:58:37 +0800

    add splash

---
+++ DemoProject/src/util/Splash.java
@@ -0,0 +1,73 @@
+package util;
+import javax.swing.*;  
+
+import java.awt.*;    
+import java.awt.image.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;    
+   
+public class Splash {
+    static Logger logger = Logger.getLogger(Splash.class.getSimpleName());
+    final SplashScreen splash = SplashScreen.getSplashScreen();
+    Rectangle splashBounds;
+    Graphics2D splashGraphics;
+    protected void initSplash() throws Exception {
+        if (splash == null) {
+            throw new Exception("no splash image specified eg. -splash:nanyou.jpg");
+        }
+        splashBounds = splash.getBounds();
+        splashGraphics = (Graphics2D) splash.createGraphics();
+        if (splashGraphics == null) throw new Exception("no SplashScreen Graphics2D");
+        splashGraphics.setColor(Color.YELLOW);
+        splashGraphics.drawRect(0, 0, splashBounds.width - 1, splashBounds.height - 1);
+    }
+    protected void updateSplash(String status, int progress) {
+        if (splash == null) return;
+        if (splashGraphics == null) return;
+        drawSplash(splashGraphics, status, progress);
+        splash.update();
+    }
+    protected void drawSplash(Graphics2D splashGraphics, String status, int progress) {
+        int barWidth = splashBounds.width*60/100;
+        splashGraphics.setComposite(AlphaComposite.Clear);
+        splashGraphics.fillRect(110, 280, splashBounds.width - 2, 20);
+        splashGraphics.setPaintMode();
+        splashGraphics.setColor(Color.RED);
+        splashGraphics.drawString(status, 150, 300);
+        splashGraphics.setColor(Color.BLACK);
+        
+        splashGraphics.setColor(Color.WHITE);
+        int width = progress*barWidth/100;
+        splashGraphics.fillRect(130, 280, width + 1, 9);
+        splashGraphics.setColor(Color.DARK_GRAY);
+        splashGraphics.fillRect(200 + width + 1, 400, barWidth - width, 9);
+    }
+    protected void loadApplication() {
+        try {
+            initSplash();
+        } catch (Exception e) {
+            logger.log(Level.WARNING, e.getMessage());
+        }
+        final String[] stages = {"dao/conncetionFacory", "dao/adminIpm", "gui/mainframe","gui/studentfram","entity/user"};
+        int stage = 0;
+        for (int i = 0; i <= 100; i += 5) {
+            String status = "Loading " + stages[stage] + "...";
+            if (splash != null) updateSplash(status, i);
+            try {
+                Thread.sleep(100);
+                if (i == 30) stage = 1;
+                else if (i == 60) stage = 2;
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+        if (splash != null) splash.close();
+        /**
+         * start here
+         */
+    }
+     
+    public static void main(String args[]) {
+        new Splash().loadApplication();
+    }
+} 
